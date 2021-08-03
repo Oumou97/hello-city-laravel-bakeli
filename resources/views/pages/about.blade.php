c05986db4bbabb40a63ad33b77ddecd0452b7bb7
Oumou Hawa Bah <oumouhawabah97@gmail.com>
2021-08-03 14:45:49 +0000

    Added  proper page titles

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,9 +1,12 @@
 @extends('app')
 
+
+@section('title', 'About Us | Hello City')
+
+
 @section('content')
     <p>Built with &hearts; by LES TEACHERS DU NET.</p>
 
     <p><a href="/">Revenir à la page d'accueil</a></p>
-
 @endsection
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,12 @@
 @extends('app')
 
+@section('title', 'Hello City')
+
+
 @section('content')
     <h1>Hello from Dakar!</h1>
 
     <p>It's currently {{ date('h:i A') }}.</p>
-
 @endsection
+
+
