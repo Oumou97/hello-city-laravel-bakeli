1a4e4ab40026f5ea5d94bdc49f5c1cef0cf1c88a
Oumou Hawa Bah <oumouhawabah97@gmail.com>
2021-08-04 16:58:13 +0000

    Add styling with Tailwind CSS

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,10 +4,12 @@
 @section('title', 'About Us | ' .config('app.name'))
 
 @section('content')
-    <img src="{{ asset('images/logoBakeli.png') }}" alt="Logo Bakeli">
+    <img src="{{ asset('images/logoBakeli.png') }}" alt="Logo Bakeli" class="my-12 rounded-full">
 
-    <p>Built with &hearts; by LES TEACHERS DU NET.</p>
+    <h2 class="mb-5 text-gray-700">Built with <span class="text-pink-500">&hearts;</span> by LES TEACHERS DU NET.</h2>
 
-    <p><a href="{{ route('home') }}">Revenir à la page d'accueil</a></p>
+    <p>
+        <a href="{{ route('home') }}" class="text-red-500 hover:text-red-600 underline">Revenir à la page d'accueil</a>
+    </p>
 @endsection
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,11 +1,11 @@
 @extends('app')
 
 @section('content')
-    <img src="{{ asset('images/drapeauSn.jpg') }}" alt="Drapeau Sénégal">
+    <img src="{{ asset('images/drapeauSn.jpg') }}" alt="Drapeau Sénégal" class="mt-12 rounded shadow-md">
 
-    <h1>Hello from Dakar!</h1>
+    <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-red-600">Hello from Dakar!</h1>
 
-    <p>It's currently {{ date('h:i A') }}.</p>
+    <p class="text-lg text-gray-800">It's currently {{ date('h:i A') }}.</p>
 @endsection
 
 
